4b82bfb138d2e737dbd602eaa352f105f2929b83
xuzhuoxi <10997324+endereating@user.noreply.gitee.com>
2022-06-03 23:11:25 +0800

    基础文件 Base File

--- djangoProject1/local_settings.py
+++ djangoProject1/local_settings.py
@@ -122,8 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
